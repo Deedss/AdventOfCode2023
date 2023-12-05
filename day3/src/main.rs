af5d677b21eb7f0f25d88170c8147663b87560eb
Gertjan Rolink <gertjan.rolink@outlook.com>
2023-12-05 22:38:11 +0100

    working day3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{linked_list, LinkedList};
+use std::collections::btree_map::IterMut;
 
 #[allow(dead_code)]
 // Idea
@@ -6,140 +6,131 @@ use std::collections::{linked_list, LinkedList};
 // Get all values from a line and their indices
 // Get all indices of symbols and check
 #[derive(Debug, Clone)]
-struct Value {
+struct Item {
     first_index: usize,
     last_index: usize,
     value: String,
-    is_symbol: bool,
-    added_to_sum: bool,
 }
 
-fn get_values(input: &str) -> Vec<Value> {
-    let mut values = Vec::new();
+impl Item {
+    fn new(index: usize, value: String) -> Self {
+        let last_index = index - 1;
+        let first_index = index - value.len();
+        Item {
+            first_index,
+            last_index,
+            value,
+        }
+    }
 
-    let mut symbol = String::new();
-    let mut number = String::new();
+    fn is_symbol(&self) -> bool {
+        self.value.chars().all(|c| !c.is_numeric())
+    }
 
-    for (index, char) in input.char_indices() {
-        if char == '.' {
-            if !symbol.is_empty() {
-                let val = Value {
-                    first_index: index - symbol.len(),
-                    last_index: index - 1,
-                    value: symbol.clone(),
-                    is_symbol: true,
-                    added_to_sum: false,
-                };
-                values.push(val);
-                symbol.clear();
-            } else if !number.is_empty() {
-                let val = Value {
-                    first_index: index - number.len(),
-                    last_index: index - 1,
-                    value: number.clone(),
-                    is_symbol: false,
-                    added_to_sum: false,
-                };
-                values.push(val);
-                number.clear();
-            }
-        } else {
-            if char.is_numeric() {
-                number.push(char);
+    fn is_number(&self) -> bool {
+        self.value.chars().all(|c| c.is_numeric())
+    }
+}
+
+fn parse_row(input: &str) -> Vec<Item> {
+    let mut items = Vec::new();
+    let mut value = String::new();
+
+    for (i, c) in input.char_indices() {
+        if c == '.' && !value.is_empty() {
+            items.push(Item::new(i, value));
+            value = String::new(); // Re-initialize the string
+        } else if c != '.' {
+            if c.is_numeric() {
+                value.push(c);
             } else {
-                symbol.push(char);
+                if !value.is_empty() {
+                    items.push(Item::new(i, value));
+                    value = String::new(); // Re-initialize the string
+                }
+                value.push(c);
+                items.push(Item::new(i + 1, value));
+                value = String::new(); // Re-initialize the string
             }
         }
     }
 
-    values
-}
-
-fn check_if_matches(prev: &Value, cur: &Value) -> bool {
-    let min_index: usize = match cur.first_index {
-        0 => 0,
-        _ => cur.first_index - 1,
-    };
-
-    let max_index: usize = cur.last_index + 1;
-    if prev.is_symbol {
-        if prev.first_index >= min_index && prev.last_index <= max_index {
-            return true;
-        }
+    // Handle the final value if it's not empty and not ended with a dot
+    if !value.is_empty() {
+        items.push(Item::new(input.len(), value));
     }
-    false
+
+    items
 }
 
-fn compare_against_prev(prev: &Vec<Value>, current: &mut Vec<Value>) {
-    for cur in current.iter_mut() {
-        if !cur.is_symbol && !cur.added_to_sum {
-            for pre in prev.iter() {
-                if check_if_matches(&pre, cur) {
-                    cur.added_to_sum = true
+fn has_matching_neighbour(curr: &Vec<Item>, value: &Item, j: usize) -> bool {
+    let mut has_neighbour = false;
+    let prev_val = curr.get(j.wrapping_sub(1));
+    let next_val = curr.get(j.wrapping_add(1));
+
+    has_neighbour = has_neighbour
+        || prev_val.is_some_and(|v| v.is_symbol() && v.last_index == (value.first_index - 1));
+    has_neighbour = has_neighbour
+        || next_val.is_some_and(|v| v.is_symbol() && v.first_index == (value.last_index + 1));
+
+    has_neighbour
+}
+
+fn has_match_in_line(line: Option<&Vec<Item>>, value: &Item) -> bool {
+    line.and_then(|l| {
+        l.iter()
+            .find(|&v| {
+                if (value.first_index == 0) {
+                    v.last_index <= (value.last_index + 1)
+                } else {
+                    v.first_index >= (value.first_index - 1)
+                        && v.last_index <= (value.last_index + 1)
+                }
+            })
+            .filter(|v| v.is_symbol())
+    })
+    .is_some()
+}
+
+fn check_numbers(lines: &[Vec<Item>]) -> u32 {
+    let mut total_sum = 0;
+
+    for (i, row) in lines.iter().enumerate() {
+        for (j, value) in row.iter().enumerate() {
+            if value.is_number() {
+                let mut has_edging_symbol = false;
+                has_edging_symbol = has_edging_symbol || has_matching_neighbour(row, value, j);
+                if i == 0 {
+                    has_edging_symbol =
+                        has_edging_symbol || has_match_in_line(lines.get(i + 1), value);
+                } else if i == lines.len() {
+                    has_edging_symbol =
+                        has_edging_symbol || has_match_in_line(lines.get(i - 1), value);
+                } else {
+                    has_edging_symbol =
+                        has_edging_symbol || has_match_in_line(lines.get(i - 1), value);
+                    has_edging_symbol =
+                        has_edging_symbol || has_match_in_line(lines.get(i + 1), value);
+                }
+
+                if has_edging_symbol {
+                    if let Ok(num) = value.value.parse::<u32>() {
+                        total_sum += num;
+                    }
                 }
             }
         }
     }
-}
 
-fn compare_against_next(next: &Vec<Value>, current: &mut Vec<Value>) {
-    for cur in current.iter_mut() {
-        if !cur.is_symbol && !cur.added_to_sum {
-            for n in next.iter() {
-                if check_if_matches(&n, cur) {
-                    cur.added_to_sum = true
-                }
-            }
-        }
-    }
-}
+    println!("{}", total_sum);
 
-fn go_over_list(rows: &mut Vec<Vec<Value>>) -> u32 {
-    let temp = rows.clone();
-    for (index, row) in rows.iter_mut().enumerate() {
-        if index == 0 {
-            compare_against_next(&temp[index + 1], row);
-        } else if index == temp.len() - 1 {
-            compare_against_prev(&temp[index - 1], row);
-        } else {
-            compare_against_prev(&temp[index - 1], row);
-            compare_against_next(&temp[index + 1], row);
-        }
-    }
-
-    // Flatten, filter, and sum the values
-    let result: Vec<Value> = rows
-        .iter()
-        .flat_map(|row| row.iter())
-        .filter(|val| val.added_to_sum)
-        .cloned() // Clone each Value (assuming Value is Clone)
-        .collect();
-
-    let mut sum = 0u32;
-
-    for value in result {
-        match value.value.parse::<u32>() {
-            Ok(num) => sum += num,
-            Err(_) => eprintln!("Failed to parse '{}' as a number.", value.value),
-        }
-        println!("{}", sum);
-    }
-
-    sum
+    total_sum
 }
 
 fn part_1(input: &str) -> u32 {
-    let mut rows: Vec<Vec<Value>> = Vec::new();
+    let rows: Vec<Vec<Item>> = input.lines().map(|row| parse_row(row)).collect();
 
-    for line in input.lines() {
-        rows.push(get_values(line));
-    }
-
-    go_over_list(&mut rows);
-
-    let sum = 0;
-
-    sum
+    check_numbers(&rows)
 }
 
 fn part_2(input: &str) -> u32 {
