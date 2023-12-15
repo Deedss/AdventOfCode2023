2f3f7e07f50f29d64409842f45d2ea35dc5ac202
Gertjan Rolink <gertjan.rolink@outlook.com>
2023-12-15 23:50:31 +0100

    brute force day5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,110 +1,113 @@
-use std::collections::HashMap;
-
 #[allow(dead_code)]
-#[derive(Debug)]
-struct Seed {
-    seed: u32,
-    soil: u32,
-    fertilizer: u32,
-    water: u32,
-    light: u32,
-    temp: u32,
-    humidity: u32,
-    location: u32,
-}
-
-impl Seed {
-    fn new(seed: u32) -> Self {
-        Seed {
-            seed,
-            soil: 0,
-            fertilizer: 0,
-            water: 0,
-            light: 0,
-            temp: 0,
-            humidity: 0,
-            location: 0,
-        }
-    }
-}
-
 #[derive(Clone, Debug)]
 struct Range {
-    destination_start: u32,
-    source_start: u32,
-    range_len: u32,
+    dest: i64,
+    src: i64,
+    range: i64,
 }
 
 impl Range {
-    fn new(destination_start: u32, source_start: u32, range_len: u32) -> Self {
-        Range {
-            destination_start,
-            source_start,
-            range_len,
-        }
+    fn new(dest: i64, src: i64, range: i64) -> Self {
+        Range { dest, src, range }
     }
 }
 
-fn parse_map(input: &str) -> HashMap<String, Vec<Range>> {
-    let mut map = HashMap::new();
-    let mut ranges = Vec::new();
-    let mut name = String::new();
-
-    for line in input.lines() {
-        if line.contains("map:") {
-            if !name.is_empty() {
-                map.insert(name.clone(), ranges.clone());
-                ranges.clear();
-            }
-            name = line.to_string();
-        } else if !line.is_empty() && !line.contains("seeds:") {
-            let temp: Vec<u32> = line
-                .split_whitespace()
-                .map(|s| s.parse::<u32>().unwrap())
-                .collect();
-            ranges.push(Range::new(temp[0], temp[1], temp[2]));
-        }
-
-        if !name.is_empty() {
-            map.insert(name.clone(), ranges.clone());
-        }
-    }
-
-    map
+fn parse_map(input: &str) -> Vec<Vec<Range>> {
+    input
+        .split("\n\n")
+        .map(|section| {
+            section
+                .lines()
+                .skip(1)
+                .map(|line| {
+                    let mut values = line.split_whitespace().map(|s| s.parse::<i64>().unwrap());
+                    let dest = values.next().unwrap();
+                    let src = values.next().unwrap();
+                    let range = values.next().unwrap();
+                    Range::new(dest, src, range)
+                })
+                .collect()
+        })
+        .collect()
 }
 
-fn parse_seeds(input: &str) -> Vec<Seed> {
+fn parse_seeds(input: &str) -> Vec<i64> {
     let line = input.lines().find(|l| l.starts_with("seeds:"));
-    let seeds: Vec<Seed> = line
+    let seeds: Vec<i64> = line
         .unwrap()
         .split_whitespace()
         .filter(|&s| !s.starts_with("seeds:"))
-        .map(|s| Seed::new(s.parse::<u32>().unwrap()))
+        .map(|s| s.parse::<i64>().unwrap())
         .collect();
 
     seeds
 }
 
-fn part_1(input: &str) -> u32 {
-    let seeds: Vec<Seed> = parse_seeds(input);
-    let map = parse_map(input);
+fn part_1(input: &str) -> i64 {
+    let mut seeds = parse_seeds(input);
+    let mut map = parse_map(input);
+    let mut lowest = i64::MAX;
 
-    println!("seeds: {:?}", seeds);
-    for i in map.iter() {
-        println!("{}:{:?}", i.0, i.1);
+    for seed in seeds.iter_mut() {
+        for ranges in map.iter_mut() {
+            for range in ranges.iter_mut() {
+                if seed >= &mut range.src && seed <= &mut (range.src + range.range - 1) {
+                    *seed = *seed + range.dest - range.src;
+                    break;
+                }
+            }
+        }
+        println!("seed:{}", seed);
+
+        if seed < &mut lowest {
+            lowest = *seed;
+        }
     }
 
-    0
+    lowest
+}
+
+fn part_2(input: &str) -> i64 {
+    let mut seeds = parse_seeds(input);
+    let mut new_seeds: Vec<i64> = Vec::new();
+    let mut iter = seeds.iter_mut();
+
+    while let Some(seed) = iter.next() {
+        if let Some(next_seed) = iter.next() {
+            let tmp: Vec<_> = (*seed..(*seed + *next_seed + 1)).collect();
+            new_seeds.extend(tmp);
+        }
+    }
+
+    let mut map = parse_map(input);
+    let mut lowest = i64::MAX;
+
+    for seed in new_seeds.iter_mut() {
+        for ranges in map.iter_mut() {
+            for range in ranges.iter_mut() {
+                if seed >= &mut range.src && seed <= &mut (range.src + range.range - 1) {
+                    *seed = *seed + range.dest - range.src;
+                    break;
+                }
+            }
+        }
+
+        if seed < &mut lowest {
+            lowest = *seed;
+        }
+    }
+
+    lowest
 }
 
 fn main() {
     let example_1 = part_1(include_str!("example.txt"));
     println!("Example part 1 : {}", example_1);
-    // let part_1 = part_1(include_str!("input.txt"));
-    // println!("Input part 1 : {}", part_1);
-    //
-    // let example_2 = part_2(include_str!("example.txt"));
-    // println!("Example part 2 : {}", example_2);
-    // let part_2 = part_2(include_str!("input.txt"));
-    // println!("Input part 2 : {}", part_2);
+    let part_1 = part_1(include_str!("input.txt"));
+    println!("Input part 1 : {}", part_1);
+
+    let example_2 = part_2(include_str!("example.txt"));
+    println!("Example part 2 : {}", example_2);
+    let part_2 = part_2(include_str!("input.txt"));
+    println!("Input part 2 : {}", part_2);
 }
